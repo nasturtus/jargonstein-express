a01854bf257d714681cd5c9a4255d7d5be68f927
nasturtus <nasturtus@gmail.com>
2018-05-04 12:25:03 +0800

    Remove extra asterisk from app get forward slash

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const app = express();
 // set up middleware / route
 let routeToJargon = require("./routes/jargon");
 
-app.get("/*", function(req, res) {
+app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 app.use("/api/jargon", routeToJargon);
